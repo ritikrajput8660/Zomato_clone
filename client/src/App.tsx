570ff2fdb7bfa3568573782c9f05c805669704d9
ritik <rajputritik8660@gmail.com>
2025-02-20 18:26:58 +0530

    routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,8 @@
 function App() {
   return (
     <>
-      <h1>Heloo</h1>
-      <h1>Heloo</h1>
-      <h1>Heloo</h1>
-      <h1>Heloo</h1>
+      <h1>Heloom pages</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga minus, expedita nihil consequatur rem amet, esse eos eius id ut voluptatem libero saepe eum aliquam.</p>
     </>
   );
 }
